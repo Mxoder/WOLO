4466d4afbcde70bcd3442a85400c1c2c7909f785
Max <max_zhang2020@outlook.com>
2024-05-29 13:19:19 +0800

    Update: 自动化配置 miniconda

---
+++ setup_miniconda.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# 创建 miniconda 目录
+mkdir -p ~/miniconda3
+
+# 下载 miniconda 安装脚本
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+
+# 安装 miniconda
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+
+# 初始化 conda
+~/miniconda3/bin/conda init bash
+
+# 配置 conda
+conda config --set show_channel_urls yes
+
+# 覆盖 ~/.condarc 文件
+cat <<EOL > ~/.condarc
+channels:
+  - defaults
+show_channel_urls: true
+default_channels:
+  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main
+  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/r
+  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/msys2
+custom_channels:
+  conda-forge: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  msys2: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  bioconda: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  menpo: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  pytorch: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  pytorch-lts: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  simpleitk: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  deepmodeling: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/
+EOL
+
+# 清理原索引
+conda clean -i -y
+
+# 配置 pip 源
+mkdir -p $HOME/.config/pip
+cat <<EOL > ~/.config/pip/pip.conf
+[global]
+index-url = https://pypi.tuna.tsinghua.edu.cn/simple
+trusted-host = pypi.tuna.tsinghua.edu.cn
+EOL
